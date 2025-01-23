061f61a095937972b5063288c0d7c7e7d9944eb6
nazarovman2024 <nazarovman2024@gmail.com>
2025-01-23 20:53:46 +0300

    firstDay

---
+++ JavaFundamentals/src/OppositeNumber.java
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/56dec885c54a926dcd001095/train/java
+public class OppositeNumber {
+    public static int opposite(int number){
+        return(number*(-1));
+    }
+}
---
+++ JavaFundamentals/src/RockPaperScissors.java
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/java
+public class RockPaperScissors {
+    public static String rps(String p1, String p2){
+        if(p1.equals(p2)) {
+            return ("Draw!");
+        } else if ((p1.equals("rock") && p2.equals("scissors"))||
+                (p1.equals("scissors") && p2.equals("paper"))||
+                (p1.equals("paper") && p2.equals("rock"))){
+            return("Player 1 won!");
+        } else {
+            return ("Player 2 won!");
+        }
+    }
+}
