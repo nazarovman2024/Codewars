e82b852095b1e29cd478b20b85d0bad6b4b99408
nazarovman2024 <nazarovman2024@gmail.com>
2025-01-24 02:21:26 +0000

    second

---
+++ JavaFundamentals/src/RemoveFirstAndLastCharacter.java
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/java
+public class RemoveFirstAndLastCharacter {
+    public static String remove(String str){
+        return(str.substring(1, str.length()-1));
+    }
+}
---
+++ JavaFundamentals/src/Upper.java
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/java
+public class Upper {
+    public static String makeUpperCase(String str){
+        return(str.toUpperCase());
+    }
+}
