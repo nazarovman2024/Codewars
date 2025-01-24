fa27882e3677477c803311ba055f5ac04ea75189
nazarovman2024 <nazarovman2024@gmail.com>
2025-01-24 11:37:58 +0300

    8kyu.2_250124.2

---
+++ JavaFundamentals/src/SmallestIntegerInTheArray.java
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/java
+import java.util.Arrays;
+
+public class SmallestIntegerInTheArray {
+    public static int find(int[] args){
+        return(Arrays.stream(args).min().getAsInt());
+    }
+}
