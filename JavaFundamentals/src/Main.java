6440b0dc10477fcae4a31904f121de81eecd8953
nazarovman2024 <nazarovman2024@gmail.com>
2025-01-27 14:20:42 +0000

    8kyu.3_250127.1

--- JavaFundamentals/src/Main.java
+++ JavaFundamentals/src/Main.java
@@ -1,8 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input: ");
+        System.out.println(ReversWords.reversWords(input.nextLine()));
     }
 }
---
+++ JavaFundamentals/src/ReversWords.java
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/java
+public class ReversWords {
+    public static String reversWords(final String original){
+        StringBuilder string = new StringBuilder();
+        int wordStart = 0;
+        for(int i=0; i<original.length(); i++){
+            if(original.charAt(i)==' '){
+                string.append(original.charAt(i));
+                wordStart = i+1;
+            } else if(i==original.length()-1) {
+                string.append(new StringBuilder(original.substring(wordStart)).reverse());
+            } else if(original.charAt(i+1)==' ') {
+                string.append(new StringBuilder(original.substring(wordStart,i+1)).reverse());
+            }
+            System.out.println(string);
+        }
+        return(string.toString());
+    }
+}
